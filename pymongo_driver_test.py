bfe66c2e9da802312558ab494ed8c601fe6eda2c
enos <enyamor@gmail.com>
2025-11-09 10:45:39 -0800

    db plays

---
+++ pymongo_driver_test.py
@@ -0,0 +1,23 @@
+from pymongo import MongoClient 
+
+try:
+    # start example code here
+
+    # end example code here
+    client = MongoClient('mongodb://127.0.0.1:27017/')
+    #client.connect()
+    #db = client.enos
+    #collection = db.collection
+
+
+    #client.db.
+    client.admin.command("ping")
+    print("Connected successfully")
+
+    # other application code
+
+    client.close()
+
+except Exception as e:
+    raise Exception(
+        "The following error occurred: ", e)
